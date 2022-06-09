2fea1a1251dd742b16ba3b7d827df5eb272247b8
Willlian Sant Anna <w.s.santanna@gmail.com>
2022-06-09 14:52:20 -0300

    Atividade resolvida do desafio.

--- desafio/Program.cs
+++ desafio/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Declarei as variáveis.
+const double UM_QUILOMETRO_EM_MILHAS = 0.6;
+
+double quilometrosPercorridosPeloVeiculo;
+
+// Capturei os quilometros rodados do veículo.
+quilometrosPercorridosPeloVeiculo = Convert.ToDouble(Console.Read());
+
+// Calculei a conversão de quilometros para milhas.
+double milhasPercorridosPeloVeiculo = quilometrosPercorridosPeloVeiculo * UM_QUILOMETRO_EM_MILHAS;
+
+// Utilize a mensagem a seguir como base do retorno esperado:
+// Seu veículo percorreu um total de XXXXXX milhas.
+Console.WriteLine($"Seu veículo percorreu um total de {milhasPercorridosPeloVeiculo} milhas.");
