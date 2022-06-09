5d680cea4f16fd154b5ddee1fbbe8a08d897a928
Willlian Sant Anna <w.s.santanna@gmail.com>
2022-06-09 14:38:31 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio/Program.cs
@@ -0,0 +1,26 @@
+﻿
+const int ANO_ATUAL = 2022;
+
+// 1. Declaração de variáveis.
+int idadeDoUsuario;
+string nomeDoUsuario;
+
+int dataDeNascimentoDoUsuario;
+
+// 2. Capturar o nome do usuário.
+Console.WriteLine("Qual seu nome?");
+nomeDoUsuario = Console.ReadLine();
+
+// 3. Capturar a idade do usuário.
+Console.WriteLine("Qual sua idade?");
+// idadeDoUsuario = int.Parse(Console.ReadLine());
+// idadeDoUsuario = Convert.ToInt16(Console.ReadLine());
+int.TryParse(Console.ReadLine(), out idadeDoUsuario); // Matheus, Carla, José Iranildo.
+
+// Calcular a data de nascimento do usuário.
+dataDeNascimentoDoUsuario = ANO_ATUAL - idadeDoUsuario;
+
+
+// Utilize a mensagem a seguir como base do retorno esperado:
+// Olá João, você nasceu em XXXX.
+Console.WriteLine($"Olá {nomeDoUsuario}, você nasceu em {dataDeNascimentoDoUsuario}.");
